a0664256073bde8a0377dcc164890a9537d1d682
Egor Borisov <padretermit666@yandex.ru>
2023-05-06 17:10:28 +0300

    one like

--- src/components/card.js
+++ src/components/card.js
@@ -3,36 +3,38 @@ import {
   elements,
 } from '../index.js';
 import { renderCard } from './modal.js';
-import { deleteLike, deliteApi, getCard, putLike,getApiProfile } from './api.js';
+import { deleteLike, deliteApi, getCard, putLike} from './api.js';
 const placeTemplate = document.querySelector('#template').content;
 const modalImage = document.querySelector('.popup_open-image__image');
 const modalText = document.querySelector('.popup_open-image__caption');
 const popupDelite = document.querySelector('.popup_delite')
-const counterLikes = placeTemplate.querySelector('.element__count')
+
 import { closePopup,openPopup} from './modal.js'; 
 const elemenDelite = document.querySelector('.popup__save-button_delite')
 const deliteForm = document.querySelector('form[name="delite"]');
 const placeElement = placeTemplate.querySelector('.element').cloneNode(true);
+const counterLikes = placeElement.querySelector('.element__count')
+
 function  createCard({ name, link,likes,id ,owner}) {
   const placeElement = placeTemplate.querySelector('.element').cloneNode(true);
   placeElement.querySelector('.element__description').textContent = name; 
   placeElement.querySelector('.element__photo').src = link; 
   placeElement.querySelector('.element__photo').alt = name; 
-  counterLikes.textContent = likes.length
-
+  counterLikes.textContent=likes.length
     //Лайки
     const like =placeElement.querySelector('.element__button');
     like.addEventListener('click', (evt) => {  
+      const counterLikesPage = document.querySelector('.element__count')
       evt.target.classList.toggle('element__button_active');
-      if(like.classList.contains('element__button_active')){
-        putLike(id)
-        counterLikes.textContent=+counterLikes.textContent+1
-      }else{
-        deleteLike(id)
-        counterLikes.textContent=+counterLikes.textContent-1  
+        if(like.classList.contains('element__button_active')){
+            putLike(id)
+            counterLikesPage.textContent=+counterLikesPage.textContent+1
+        }else{
+          deleteLike(id)
+          counterLikesPage.textContent=+counterLikesPage.textContent-1
       }
       }); 
-  
+    
 
 if(owner === 'e73ca1412a678dbe12a1e470'){
         //Удаление карточек
